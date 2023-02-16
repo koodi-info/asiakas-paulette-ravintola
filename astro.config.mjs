02e428e2659ca422717c9f1c854f14f924523c0a
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2023-02-16 13:37:34 +0200

    new build test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
-
-
 import vue from "@astrojs/vue";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
