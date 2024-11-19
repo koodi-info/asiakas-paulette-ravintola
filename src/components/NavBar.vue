28698c110e3cbdb49c431c9e96cb534832c805b9
koodi.info <61112787+koodi-info@users.noreply.github.com>
2024-11-19 17:57:49 +0200

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,11 +16,13 @@
         <a href="https://v2.tableonline.fi/instabook/bookings/yUijLmr" target="_blank">{{data.tablereservation}}</a>
         <a href="https://www.tableonline.fi/fi/helsinki/ravintola-paulette/1344/giftcard" target="_blank">{{data.giftcard}}</a>
         <a href="#lounaslista" @click="navigationToggle()">{{data.lunch}}</a>
+        <a href="#joululounas" @click="navigationToggle()">Joululounas</a>
+        <a href="#uudenvuoden-menu" @click="navigationToggle()">Uudenvuoden menu</a>
         <a href="#alacartelista" @click="navigationToggle()">{{data.alacarte}}</a>
         <a href="#restaurants" @click="navigationToggle()">{{data.restaurants}}</a>
         <a href="#catering" @click="navigationToggle()">{{data.catering}}</a>
         <a href="#about-us" @click="navigationToggle()">{{data.we}}</a>
-        <a href="#yhteydenotto" @click="navigationToggle()">{{data.yhteydenotto}}></a>
+        <a href="#yhteydenotto" @click="navigationToggle()">{{data.yhteydenotto}}</a>
         <slot></slot>
       </div>
     </div>
@@ -54,19 +56,18 @@ export default {
 <style lang="postcss">
 .navbar {
   @apply z-50 w-full fixed;
-  transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
-    rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+  transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
   transform-style: preserve-3d;
   background-color: var(--color05);
   &__container {
-    @apply md:container md:mx-auto flex flex-col md:flex-row justify-center md:justify-between md:items-center px-4 py-2;
+    @apply xl:container xl:mx-auto flex flex-col xl:flex-row justify-center xl:justify-between xl:items-center px-4 py-2;
   }
   &__actions {
-    @apply flex flex-row items-center w-full justify-between md:w-0;
+    @apply flex flex-row items-center w-full justify-between xl:w-0;
   }
   &__items {
-    @apply md:flex md:space-x-4  font-serif font-extrabold md:text-xs uppercase;
-    @media (max-width: theme("screens.md")) {
+    @apply xl:flex xl:space-x-4  font-serif font-extrabold xl:text-xs uppercase;
+    @media (max-width: theme("screens.xl")) {
       @apply opacity-0 hidden; 
     }
     color: var(--color03);
@@ -78,17 +79,17 @@ export default {
     }
   }
   &__brand {
-    @apply flex flex-col flex-shrink-0 order-first text-center md:text-left;
+    @apply flex flex-col flex-shrink-0 order-first text-center xl:text-left;
     img {
       @apply w-28 my-2;
     }
   }
   &__togglebutton {
-    @apply md:hidden font-serif font-extrabold md:text-xs md:text-base uppercase;
+    @apply xl:hidden font-serif font-extrabold xl:text-xs xl:text-base uppercase;
     color: var(--color03);
   }
   &__mobile {
-    @media (max-width: theme("screens.md")) {
+    @media (max-width: theme("screens.xl")) {
       @apply opacity-100 block;
       margin-top: 16px;
       border-top-width: 1px;
